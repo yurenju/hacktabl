aa140a90d347442ea33291a8bc320d0feb08113e
MrOrz <johnsonliang7@gmail.com>
2014-08-29 19:38:13 +0800

    Change karma.conf to make travis happy. Add firefox when on travis.

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -2,7 +2,7 @@
 // Generated on Tue May 13 2014 12:20:09 GMT+0800 (CST)
 
 module.exports = function(config) {
-  config.set({
+  var configuration = {
 
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '../',
@@ -75,9 +75,21 @@ module.exports = function(config) {
     // browsers: ['Chrome', 'Safari', 'Firefox'],
     browsers: ['Chrome'],
 
+    customLaunchers: {
+      Chrome_travis_ci: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
+    },
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
     singleRun: false
-  });
+  };
+
+  if(process.env.TRAVIS){
+    configuration.browsers = ['Chrome_travis_ci', 'Firefox'];
+  }
+
+  config.set(configuration);
 };
